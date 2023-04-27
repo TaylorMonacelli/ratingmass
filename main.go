b2f7eb8a861555078744a25083057550e27e6ffa
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-26 21:56:13 -0700

    real men dont make git commit comments

--- main.go
+++ main.go
@@ -1,46 +1,46 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"os"
-
-	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "json-print",
-	Short: "Print a JSON object to stdout",
-	Run:   printJson,
-}
-
-var jsonObject string
-
-func init() {
-	rootCmd.PersistentFlags().StringVar(&jsonObject, "json", "", "The JSON object to print")
-	rootCmd.MarkPersistentFlagRequired("json")
-}
-
 func main() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-}
+	var buf bytes.Buffer
+	reader := bufio.NewReader(os.Stdin)
 
-func printJson(cmd *cobra.Command, args []string) {
-	var obj interface{}
-	err := json.Unmarshal([]byte(jsonObject), &obj)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				buf.WriteString(line)
+				break // end of the input
+			} else {
+				fmt.Println(err.Error())
+				os.Exit(1) // something bad happened
+			}
+		}
+		buf.WriteString(line)
+
+	}
+
+	fmt.Printf("valid json? %v\n", json.Valid(buf.Bytes()))
+
+	type MedicalRecord struct {
+		Name string `json:"name"`
+		Age  int    `json:"age"`
+	}
+
+	var record MedicalRecord
+	err := json.Unmarshal(buf.Bytes(), &record)
 	if err != nil {
-		fmt.Println("Failed to parse JSON object", err)
-		return
+		fmt.Println(err.Error())
+		os.Exit(1) // something bad happened
 	}
 
-	out, err := json.Marshal(obj)
-	if err != nil {
-		fmt.Println("Failed to serialize JSON object", err)
-		return
-	}
-
-	fmt.Println(string(out))
+	fmt.Printf("name: %s, age: %d\n", record.Name, record.Age)
 }
